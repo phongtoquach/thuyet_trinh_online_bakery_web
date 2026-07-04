bc794bb42ee3f63c2f72845b55fd8d30f81c9653
Phong Quach <phongqt@fpt.com>
2026-07-04 15:36:21 +0700

    up file html trang home, CSS, vai image cho product, js data products

---
+++ scripts/products_data.js
@@ -0,0 +1,18 @@
+let productsList = [
+    {
+        id: 123,
+        name: "Bánh Mousse Dưa Lưới Minty",
+        price: 545000,
+        images: [
+            "images/mousese-dua-luoi-minty-1.jpg",
+            "images/mousese-dua-luoi-minty-2.jpg",
+            "images/mousese-dua-luoi-minty-3.jpg",
+            "images/mousese-dua-luoi-minty-4.jpg"
+        ],
+        shortDescription: "<p>Mousse Dưa Lưới là sự kết hợp hài hòa giữa bạt vani mềm nhẹ và mousse dưa lưới thanh mát quyện cùng cream cheese béo dịu. Điểm nhấn nằm ở lớp nhân bạc hà the mát, mang đến cảm giác tươi mới đầy thú vị trong từng muỗng bánh, tạo nên hương vị tươi mát và tinh tế cho những ngày hè.</p>",
+        description: "<p><strong>Hương vị:</strong> Thanh mát - Ngọt dịu - Béo nhẹ</p><p><strong>Cấu trúc bánh:</strong></p><p>Phần thân bánh gồm 3 lớp xen kẽ các lớp:</p><ul><li><p>Bạt bông lan vani truyền thống.</p></li><li><p>Mousse dưa lưới kết hợp với cream cheese béo nhẹ.</p></li><li><p>Nhân bạc hà tươi mát.</p></li></ul><p>Phần trang trí: Kết hợp decor dưa lưới tươi mát, mousse sữa chua, kem trang trí tuyền thống và chocolate tạo hình.</p><p><strong>Bảo quản:</strong> Bánh nên được dùng trong ngày và ngon hơn khi bảo quản lạnh trước khi thưởng thức.</p><p><strong>Phụ kiện tặng kèm:</strong></p><ul><li><p>1 dao cắt bánh</p></li><li><p>1 bộ dĩa và muỗng</p></li><li><p>Hộp nến nhỏ</p></li></ul>",
+        isFeatured: true,
+        relatedProductIds: [12, 36, 57],
+        inStock: true
+    }
+];
