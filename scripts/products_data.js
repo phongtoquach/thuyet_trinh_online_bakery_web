d9add39858797baff65be4fd626e1b71d2f5f365
Phong Quach <phongqt@fpt.com>
2026-07-07 13:49:18 +0700

    home page : load featured products bang JS

--- scripts/products_data.js
+++ scripts/products_data.js
@@ -14,5 +14,52 @@ let productsList = [
         isFeatured: true,
         relatedProductIds: [12, 36, 57],
         inStock: true
-    }
+    },
+    {
+        id: 124,
+        name: "Bánh Mousse Thanh Nhãn",
+        price: 565000,
+        images: [
+            "images/mousse-thanh-nhan.jpg",
+            "images/mousse-thanh-nhan-2.jpg",
+            "images/mousse-thanh-nhan-3.jpg",
+            "images/mousse-thanh-nhan-4.jpg",
+            "images/mousse-thanh-nhan-5.jpg"
+        ],
+        shortDescription: "<p>Mousse Thanh Nhãn là sự giao thoa giữa cốt choco đậm đà và mousse nhãn ngọt thanh, mát mịn. Nhân bánh gây ấn tượng với thịt nhãn giòn sần sật quyện cùng hạt dẻ béo bùi lạ miệng, thơm ngậy. Vẻ ngoài chỉn chu với nhãn tươi và hoa thủ công, mang đến trải nghiệm vị giác tinh tế, thanh mát cho ngày hè</p>",
+        description: "<p><strong>Hương vị:</strong>&nbsp;Thanh mát - Ngọt dịu - Béo nhẹ</p><p><strong>Cấu trúc bánh:</strong></p><p>Phần thân bánh gồm 3 lớp xen kẽ các lớp:</p><ul><li><p>Bạt bông lan choco.</p></li><li><p>Mousse nhãn mềm mát mịn vị ngọt thanh.</p></li><li><p>Lớp nhân nhãn có thịt nhãn giòn sần sật, lớp nhân hạt dẻ béo bùi.</p></li></ul><p>Phần trang trí: Trên mặt bánh decor kem topping, nhãn lon, hoa gumpaste, lá húng lủi, choco.</p><p><strong>Bảo quản:</strong> Bánh nên được dùng trong ngày và ngon hơn khi bảo quản lạnh trước khi thưởng thức.</p><p><strong>Phụ kiện tặng kèm:</strong></p><ul><li><p>1 dao cắt bánh</p></li><li><p>1 bộ dĩa và muỗng</p></li><li><p>Hộp nến nhỏ</p></li></ul><p></p>",
+        isFeatured: true,
+        relatedProductIds: [12, 36, 57],
+        inStock: true
+    },
+    {
+        id: 125,
+        name: "Bánh Mousse Ngọc Nhãn",
+        price: 565000,
+        images: [
+            "images/mousse-ngoc-nhan.jpg",
+            "images/mousse-ngoc-nhan-2.jpg",
+            "images/mousse-ngoc-nhan-3.jpg",
+            "images/mousse-ngoc-nhan-4.jpg",
+            "images/mousse-ngoc-nhan-5.jpg"
+        ],
+        shortDescription: "<p>Mousse Ngọc Nhãn gây ấn tượng với lớp thạch trong veo bao bọc nhãn độc đáo. Cấu trúc bánh kết hợp hài hòa giữa bạt chocolate đắng nhẹ, mousse nhãn mịn mượt ngọt thanh và điểm nhấn nhân nhãn giòn sần sật quyện cùng hạt dẻ béo bùi. Sự đối lập thú vị giữa các tầng kết cấu và vị trái cây thanh mát mang đến một trải nghiệm tinh tế</p>",
+        description: "<p><strong>Hương vị:</strong>&nbsp;Thanh mát - Ngọt dịu - Béo nhẹ</p><p><strong>Cấu trúc bánh:</strong></p><p>Phần thân bánh gồm 3 lớp xen kẽ các lớp:</p><ul><li><p>Bạt bông lan choco.</p></li><li><p>Mousse nhãn mềm mát mịn vị ngọt thanh.</p></li><li><p>Lớp nhân nhãn có thịt nhãn giòn sần sật, lớp nhân hạt dẻ béo bùi.</p></li></ul><p>Phần trang trí: Trên mặt bánh decor kem topping, nhãn lon, hoa gumpaste, lá húng lủi, choco.</p><p><strong>Bảo quản:</strong> Bánh nên được dùng trong ngày và ngon hơn khi bảo quản lạnh trước khi thưởng thức.</p><p><strong>Phụ kiện tặng kèm:</strong></p><ul><li><p>1 dao cắt bánh</p></li><li><p>1 bộ dĩa và muỗng</p></li><li><p>Hộp nến nhỏ</p></li></ul><p></p>",
+        isFeatured: true,
+        relatedProductIds: [12, 36, 57],
+        inStock: true
+    },
+    // {
+    //     id: 0,
+    //     name: "",
+    //     price: 565000,
+    //     images: [
+            
+    //     ],
+    //     shortDescription: "",
+    //     description: "",
+    //     isFeatured: true,
+    //     relatedProductIds: [],
+    //     inStock: true
+    // }
 ];
---
+++ scripts/products_main.js
@@ -0,0 +1,35 @@
+/**
+ * Ham tao HTML danh sach product dua theo array productsData truyen vao
+ * Ham nay se xai chung cho tat ca cac section can show danh sach product
+ */
+function showProductsList(productsData, containerId) {
+    let productsHtml = "";
+
+    for (let i=0; i < productsData.length; i++) {
+        // open div product-card
+        productsHtml += '<div class="product-card">';
+
+        productsHtml += '<div class="product-card-image"><img src="' + productsData[i].images[0] + '" alt="' + productsData[i].name + '"><span class="product-card-tag">Nổi bật</span></div>';
+        
+        // open div product-card-body
+        productsHtml += '<div class="product-card-body"><h3>' + productsData[i].name + '</h3>';
+        productsHtml += '<div class="short-desc">' + productsData[i].shortDescription + '</div>';
+        productsHtml += '<div class="product-card-footer"><span class="product-price">' + productsData[i].price.toLocaleString("vi-VN") + 'đ</span></div>';
+        
+        // generate buttons : Add to cart, xem chi tiet
+        productsHtml += '<div class="product-card-actions">';
+        productsHtml += '<button class="add-to-cart-btn" onclick="addToCart(' + productsData[i].id + ')"><i class="fas fa-cart-plus"></i> Add to Cart</button>';
+        productsHtml += '<a href="product_details.html?product_id=' + productsData[i].id + '" class="view-details-btn" target="_blank"><i class="fas fa-eye"></i> Details</a>';
+        productsHtml += "</div>";
+
+        // close div product-card-body
+        productsHtml += "</div>";
+
+        // close div product-card
+        productsHtml += "</div>";
+    }
+
+    document.getElementById(containerId).innerHTML = productsHtml;
+}
+
+showProductsList(productsList, "featuredProductsBox");
